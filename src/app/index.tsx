f175b70a361086b69bd3d5d12f6139c77163e57b
Henrincode <spellctro@gmail.com>
2026-06-17 03:13:04 -0300

    feat: creating src directory

---
+++ src/app/index.tsx
@@ -0,0 +1,24 @@
+import { View, Text, StyleSheet } from "react-native"
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>
+                Olá mundo!
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#1A1A1A"
+    },
+    text: {
+        color: "#676767",
+        fontSize: 40
+    }
+})
